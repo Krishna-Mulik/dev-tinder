cea469b212deb1fe08b0401abdfbff928f13a189
Krishna-Mulik <krishnamulik24@gmail.com>
2026-03-07 19:44:54 +0530

    basic layout added.

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,10 @@
-import Navbar from './components/Navbar'
 import './index.css'
 
 function App() {
 
     return (
         <>
-            <Navbar />
+            <h1>HomePage</h1>
         </>
     )
 }
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import App from './App.tsx'
+import RootLayout from './layouts/RootLayout.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
-    </StrictMode>,
-)
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<RootLayout />} >
+                    <Route index element={<App />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode >
+);
